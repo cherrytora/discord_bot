3051ba23338772c853b8a8dc7be733d648a05d4d
cherrytora <yiren513@gmail.com>
2022-10-20 23:53:20 +0800

    Update Cog

--- Code/cmds/jeff.py
+++ Code/cmds/jeff.py
@@ -1,4 +1,10 @@
 from discord.ext import commands
+import os 
+from dotenv import load_dotenv
+import discord
+
+# Load .env
+load_dotenv()
 
 ## 把剛剛在get_start.py中url_pict和Jeff兩個指令搬過來，避免class名稱和function相同，所以把原本的Jeff指令改成song
 class Jeff(commands.Cog):
@@ -9,12 +15,40 @@ class Jeff(commands.Cog):
     async def url_pict(self, ctx):
         pic_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ090g9KzhdPZifbHKyKMRUARuaAbrA753IPg&usqp=CAU'
         await ctx.send(pic_url)
-
-    # 傳送Why don't you stay 歌曲 Youtube網址
+    
     @commands.command()
-    async def song(self, ctx):
-        jeff_url = 'https://youtu.be/6ZF8RXvV9sQ'
-        await ctx.send(jeff_url)
+    async def play_list(self, ctx):
+        embed=discord.Embed(title="Jeff Satur channel", url="https://www.youtube.com/channel/UCSFWkjQRmSJoz9QQ4lkwlDA", color=0x0eacfb)
+        embed.set_author(name="Jeff Satur", url="https://www.youtube.com/channel/UCSFWkjQRmSJoz9QQ4lkwlDA", icon_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGQucI0UakUD4S14jACm42rQXg191f7RIG7A&usqp=CAU")
+        embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGQucI0UakUD4S14jACm42rQXg191f7RIG7A&usqp=CAU")
+        for i, j in zip(["Hide", "Loop", "Why", ],["Jeff Satur - แค่เงา (Hide)【Official Music Video】", "Jeff Satur - วันนี้คือพรุ่งนี้ของเมื่อวาน (Loop)【Official Music Video】", "Jeff Satur - Why Don't You Stay (WorldTour Ver.)[Official MV]"]):
+            embed.add_field(name=f'輸入 ?s {i}', value=j, inline=False)
+        await ctx.send(embed=embed)
+
+    @commands.group()
+    async def s(self,ctx):
+       pass
+
+    @s.command()
+    async def Hide(self,ctx):
+        embed=discord.Embed(title="Jeff Satur - แค่เงา (Hide)【Official Music Video】", url="https://www.youtube.com/watch?v=k9aI7dHj8GM", color=0x0eacfb)
+        embed.set_thumbnail(url="https://i.ytimg.com/vi/k9aI7dHj8GM/sddefault.jpg")
+        await ctx.send(embed=embed)
+    
+    @s.command()
+    async def Loop(self,ctx):
+        embed=discord.Embed(title="Jeff Satur - วันนี้คือพรุ่งนี้ของเมื่อวาน (Loop)【Official Music Video】", url="https://www.youtube.com/watch?v=AfeEOrQHBAo", color=0x0eacfb)
+        embed.set_thumbnail(url="https://i.ytimg.com/vi/k9aI7dHj8GM/sddefault.jpg")
+        await ctx.send(embed=embed)
+    
+    @s.command()
+    async def Why(self,ctx):
+        embed=discord.Embed(title="Jeff Satur - Why Don't You Stay (WorldTour Ver.)[Official MV]", url="https://youtu.be/6ZF8RXvV9sQ", color=0x0eacfb)
+        embed.set_thumbnail(url="https://i.ytimg.com/vi/6ZF8RXvV9sQ/sddefault.jpg")
+        await ctx.send(embed=embed)
+        
+    # # 來建立一個Jeff播放清單，讓使用者可以自行選擇歌曲
+    # # 用youtube api去更新他的播放清單ＸＤ
 
 async def setup(bot):
    await bot.add_cog(Jeff(bot))
--- Docs/cogs.md
+++ Docs/cogs.md
@@ -1,1 +1,22 @@
 # Cogs => discord 2.0
+- 小提醒：discord 2.0把cog的功能改成async了喔！有些教學是舊的版本，嘗試了之後發現舊版本已經不能再建立cog功能了～
+
+[youtube api 教學](https://medium.com/%E5%BD%BC%E5%BE%97%E6%BD%98%E7%9A%84%E8%A9%A6%E7%85%89-%E5%8B%87%E8%80%85%E7%9A%84-100-%E9%81%93-swift-ios-app-%E8%AC%8E%E9%A1%8C/101-%E4%BD%BF%E7%94%A8-youtube-data-api-%E6%8A%93%E5%8F%96%E6%9C%89%E8%B6%A3%E7%9A%84-youtuber-%E5%BD%B1%E7%89%87-mv-d05c3a0c70aa)
+[Embeds | Discord.py 2.0](https://www.youtube.com/watch?v=urLZoyLUDdE)
+[Embeds code 生成器](https://cog-creators.github.io/discord-embed-sandbox/)
+
+
+
+.....還在努力生內容中
+
+run bot 改成這樣
+```python
+async def main():
+    await load_cogs()
+    await bot.start(os.getenv('bot_token'))
+
+if __name__ == '__main__':
+    asyncio.run(main())
+```
+
+Back to [README](../README.md)
